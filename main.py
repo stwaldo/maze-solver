c980e61861465ab990600377beb0ccdec1356850
stwaldo <stwaldo@gmail.com>
2024-12-09 11:31:57 -0500

    Implement maze solving functionality with recursive backtracking

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from maze import Maze
 def main():
     win = Window(800, 600)
     maze = Maze(50, 50, 10, 10, 50, 50, win)
+    maze.solve()
     win.wait_for_close()
     
 if __name__ == "__main__":
--- maze.py
+++ maze.py
@@ -86,3 +86,34 @@ class Maze:
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._cells[i][j].visited = False
+                
+    def solve(self):
+        return self._solve_r(0, 0)
+    
+    def _solve_r(self, i, j):
+        self._animate()
+        cell = self._cells[i][j]
+        cell.visited = True
+        if i == self._num_cols - 1 and j == self._num_rows - 1:
+            return True
+        if i > 0 and not self._cells[i - 1][j].has_right_wall and not self._cells[i - 1][j].visited:
+            cell.draw_move(self._cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            cell.draw_move(self._cells[i - 1][j], undo=True)
+        if j > 0 and not self._cells[i][j - 1].has_bottom_wall and not self._cells[i][j - 1].visited:
+            cell.draw_move(self._cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            cell.draw_move(self._cells[i][j - 1], undo=True)
+        if i < self._num_cols - 1 and not self._cells[i][j].has_right_wall and not self._cells[i + 1][j].visited:
+            cell.draw_move(self._cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            cell.draw_move(self._cells[i + 1][j], undo=True)
+        if j < self._num_rows - 1 and not self._cells[i][j].has_bottom_wall and not self._cells[i][j + 1].visited:
+            cell.draw_move(self._cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            cell.draw_move(self._cells[i][j + 1], undo=True)
+        return False
